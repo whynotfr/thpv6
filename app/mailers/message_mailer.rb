2cc81879e697941b90f90b8ccf1c082a1926422b
whynotfr <charles@offwefly.co>
2017-08-13 00:12:08 +0200

    mailer work

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -6,11 +6,11 @@ class MessageMailer < ApplicationMailer
   #   en.message_mailer.contact_me.subject
   #
   def contact_me(message)
-    @greeting = "Hi"
-    @body = message.body
-    mail(
-    	to: "bonjour@thehackingproject.org", from: 'charles@thehackingproject.org'
-    	delivery_method_options: {api_key: 'ed9f24040f5022c12d434dd1413432fc', secret_key: 'abc390060a9cded9516ced2a67c81ba3'}
-    	)
+    
+    @prenom = message.first_name
+    @nom = message.last_name
+    @mail = message.email 
+    @body = message.body 
+    mail to: "bonjour@thehackingproject.org", from: 'charles@thehackingproject.org'
   end
 end
