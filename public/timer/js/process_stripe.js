99303f3c64ef6bcbe0c875f71d3198bee7c266c9
whynotfr <charles@offwefly.co>
2017-08-24 12:38:59 +0200

    sivousplait

---
+++ public/timer/js/process_stripe.js
@@ -0,0 +1,15 @@
+ //Gestion du process d'inscritpion.
+jQuery(document).ready(function(){
+//première partie avec acceptation CGV
+$("#bouton-cgv").click(function(){
+    $("#inscription-init").fadeOut("slow");
+    	$("#bouton-cgv").fadeOut("slow");
+        	$("#paiement").css("display", "block");
+            	$(window).scrollTop(0);
+});
+//deuxième partie avec paiement
+
+//troisième partie avec remplissage questionnaire
+
+//Félicitation pour notre nousveau moussaillon
+});
