31d3d93750aa4f3b4c086144d6da0dc54c4ba88d
whynotfr <charles@offwefly.co>
2017-08-24 10:03:32 +0200

    test sans stripe

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -21,24 +21,5 @@ class StaticController < ApplicationController
   def realize
   end
 
-  def paiement
-    # Amount in cents
-    @amount = 100
-
-    customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
-    )
-
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'paiment enregistrement',
-      :currency    => 'eur'
-    )
-
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
-  end
+  
 end
--- config/initializers/stripe.rb
+++
@@ -1,6 +0,0 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['pk_live_3mU8BB8PWIio9HPrRXq5ytHG'],
-  :secret_key      => ENV['sk_live_coQLDE0XiqEmneLn6ODSRAlh']
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
