e887997f296fb1aa725601852b4a60d535188a61
whynotfr <charles@offwefly.co>
2017-08-24 10:11:09 +0200

    stripe2

--- public/timer/js/main.js
+++ public/timer/js/main.js
@@ -1,17 +1,4 @@
- //Gestion du process d'inscritpion.
-jQuery(document).ready(function(){
-//première partie avec acceptation CGV
-$("#bouton-cgv").click(function(){
-    $("#inscription-init").fadeOut("slow");
-        $("#paiement").css("display", "block");
-            $(window).scrollTop(0);
-});
-//deuxième partie avec paiement
 
-//troisième partie avec remplissage questionnaire
-
-//Félicitation pour notre nousveau moussaillon
-});
 
 
 
